dddf601645bf977f369dd54e2cf27c9784661bba
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-05-12 02:10:29 +0000

    Add more testing, fix node_dump.

--- test/main.c
+++ test/main.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include "../src/btree.h"
 
-void node_dump(node_t * inNode) {
+void node_dump(node_t * inNode, uint16_t inLevel) {
     int i;
+    for(i = 0; i < inLevel; i++) {
+        printf("\t");
+    }
     printf("[");
     for(i = 0; i < inNode->numKeys; i++) {
         printf(" %02ld ",inNode->keys[i]);
     }
-    printf("]\n");
+    printf("]");
 
     if(!node_is_leaf(inNode)) {
         for(i = 0; i <= inNode->numKeys; i++) {
-            node_dump(inNode->children[i]);
+            printf("\n");
+            node_dump(inNode->children[i], inLevel+1);
         }
     }
 }
 
 void btree_dump(btree_t * inTree) {
     printf("B-Tree (Height %d):\n", inTree->height);
-    node_dump(inTree->root);
+    node_dump(inTree->root, 0);
+    printf("\n");
 }
 
 int main(void) {
@@ -36,6 +41,33 @@ int main(void) {
     btree_dump(tree);
 
     btree_insert(tree, 8, 8);
+    btree_dump(tree);
+
+    btree_insert(tree, 10, 10);
+    btree_dump(tree);
+
+    btree_insert(tree, 1, 1);
+    btree_dump(tree);
+
+    btree_insert(tree, 3, 3);
+    btree_dump(tree);
+
+    btree_insert(tree, 5, 5);
+    btree_dump(tree);
+
+    btree_insert(tree, 7, 7);
+    btree_dump(tree);
+
+    btree_insert(tree, 9, 9);
+    btree_dump(tree);
+
+    btree_insert(tree, 11, 11);
+    btree_dump(tree);
+
+    btree_insert(tree, 12, 12);
+    btree_dump(tree);
+
+    btree_insert(tree, 13, 13);
     btree_dump(tree);
 
     btree_destroy(tree);
