06025cd3cda67f3cc00b48998120c77d3557f9ea
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-05-19 03:04:59 +0000

    Fix test iterations and timeouts.

--- test/main.c
+++ test/main.c
@@ -4,12 +4,12 @@
 #include "insert.h"
 
 MunitSuite sub_suites[] = { 
-    { "/create", create_tests, NULL, 100, MUNIT_SUITE_OPTION_NONE },
-    { "/destroy", destroy_tests, NULL, 100, MUNIT_SUITE_OPTION_NONE },
+    { "/create", create_tests, NULL, 1, MUNIT_SUITE_OPTION_NONE },
+    { "/destroy", destroy_tests, NULL, 1, MUNIT_SUITE_OPTION_NONE },
     { "/insert", insert_tests, NULL, 1, MUNIT_SUITE_OPTION_NONE },
     { NULL, NULL, NULL, 0, MUNIT_SUITE_OPTION_NONE } 
 };
-static const MunitSuite suite = { "/btree", NULL, sub_suites, 10, MUNIT_SUITE_OPTION_NONE };
+static const MunitSuite suite = { "/btree", NULL, sub_suites, 15, MUNIT_SUITE_OPTION_NONE };
 
 int main(int argc, char * const argv[]) {
     return munit_suite_main(&suite, NULL, argc, argv);
