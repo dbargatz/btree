65a954eb710354a35176e9cc8a5e58c82332e0b1
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-05-24 23:21:37 +0000

    Reorganize search tests.

--- test/search.c
+++ test/search.c
@@ -1,28 +1,16 @@
 #include "search.h"
-#include <stdio.h>
 #include "../src/btree.h"
 
-#define DEGREE_MAX     (100)
-#define TEST_KEY_MIN   (0)
-#define TEST_KEY_MAX   (UINT16_MAX)
-#define RANDOM_KEY_MIN (TEST_KEY_MAX+1)
-#define RANDOM_KEY_MAX (UINT64_MAX)
-
-void * search_setup(const MunitParameter inParams[], void * inFixture);
-void search_teardown(void * inFixture);
+#define TEST_DEGREE_MAX  (100)
+#define TEST_KEY_MIN     (0)
+#define TEST_KEY_MAX     (UINT16_MAX)
+#define RANDOM_KEY_MIN   (TEST_KEY_MAX+1)
+#define RANDOM_KEY_MAX   (UINT64_MAX)
 
 MunitResult null_tree_fails (const MunitParameter inParams[], void * inFixture);
 MunitResult null_root_fails (const MunitParameter inParams[], void * inFixture);
 MunitResult not_found_fails (const MunitParameter inParams[], void * inFixture);
 MunitResult found_succeeds (const MunitParameter inParams[], void * inFixture);
-
-MunitTest search_tests[] = {
-    { "/null_tree_fails", null_tree_fails, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
-    { "/null_root_fails", null_root_fails, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
-    { "/not_found_fails", not_found_fails, search_setup, search_teardown, MUNIT_TEST_OPTION_NONE, NULL },
-    { "/found_succeeds", found_succeeds, search_setup, search_teardown, MUNIT_TEST_OPTION_NONE, NULL },
-    { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
-};
 
 uint64_t search_rand_uint64(uint64_t inMin, uint64_t inMax) 
 {
@@ -32,6 +20,39 @@ uint64_t search_rand_uint64(uint64_t inMin, uint64_t inMax)
     } while(inMin > key || inMax < key || key == INVALID_KEY_VALUE_SENTINEL);
     return key;
 }
+
+void * search_setup(const MunitParameter inParams[], void * inFixture) {
+    // Create a B-tree with a relatively small min degree to exercise search, but 
+    // not take forever.
+    int i;
+    uint64_t key = INVALID_KEY_VALUE_SENTINEL;
+    uint16_t t = (uint16_t)munit_rand_int_range(DEGREE_MIN, TEST_DEGREE_MAX);
+    btree_t * tree = btree_create(t);
+
+    // Generate and insert enough keys to cause multiple splits.
+    int32_t numInserts = munit_rand_int_range(tree->t * 4, tree->t * 16);
+
+    // Insert enough keys to cause multiple splits. Exclude the test key range,
+    // and use the key as the value.
+    for(i = 0; i < numInserts; i++) {
+        key = search_rand_uint64(RANDOM_KEY_MIN, RANDOM_KEY_MAX);
+        btree_insert(tree, key, key);
+    }
+
+    return tree;
+}
+
+void search_teardown(void * inFixture) {
+    btree_destroy((btree_t *)inFixture);
+}
+
+MunitTest search_tests[] = {
+    { "/null_tree_fails", null_tree_fails, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/null_root_fails", null_root_fails, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/not_found_fails", not_found_fails, search_setup, search_teardown, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/found_succeeds", found_succeeds, search_setup, search_teardown, MUNIT_TEST_OPTION_NONE, NULL },
+    { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
+};
 
 MunitResult null_tree_fails (const MunitParameter inParams[], void * inFixture) 
 {
@@ -53,11 +74,9 @@ MunitResult null_root_fails (const MunitParameter inParams[], void * inFixture)
 MunitResult not_found_fails (const MunitParameter inParams[], void * inFixture)
 {
     // Generate a test key guaranteed to NOT be in the tree and search for it.
-    munit_log(MUNIT_LOG_INFO, "Starting not_found_fails");
     btree_t * tree = (btree_t *)inFixture;
     uint64_t key = (uint64_t)munit_rand_int_range(TEST_KEY_MIN, TEST_KEY_MAX);
     uint64_t value = btree_search(tree, key);
-    munit_log(MUNIT_LOG_INFO, "Searched not_found_fails");
 
     // Make sure the key wasn't found. 
     munit_assert_uint64(value, ==, INVALID_KEY_VALUE_SENTINEL);
@@ -77,29 +96,4 @@ MunitResult found_succeeds (const MunitParameter inParams[], void * inFixture)
     value = btree_search(tree, key);
     munit_assert_uint64(value, ==, key);
     return MUNIT_OK;
-}
-
-void * search_setup(const MunitParameter inParams[], void * inFixture) {
-    // Create a B-tree with a relatively small order to exercise search, but not
-    // take forever.
-    int i;
-    uint64_t key = INVALID_KEY_VALUE_SENTINEL;
-    uint16_t t = (uint16_t)munit_rand_int_range(DEGREE_MIN, DEGREE_MAX);
-    btree_t * tree = btree_create(t);
-
-    // Generate and insert enough keys to cause multiple splits.
-    int32_t numInserts = munit_rand_int_range(tree->t * 4, tree->t * 16);
-
-    // Insert enough keys to cause multiple splits. Exclude the test key range,
-    // and use the key as the value.
-    for(i = 0; i < numInserts; i++) {
-        key = search_rand_uint64(RANDOM_KEY_MIN, RANDOM_KEY_MAX);
-        btree_insert(tree, key, key);
-    }
-
-    return tree;
-}
-
-void search_teardown(void * inFixture) {
-    btree_destroy((btree_t *)inFixture);
 }
