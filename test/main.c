f6bde6892170afab49819d8e6ed1c1fb53058628
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-05-25 01:07:18 +0000

    Increase test iterations.

--- test/main.c
+++ test/main.c
@@ -11,7 +11,7 @@ MunitSuite sub_suites[] = {
     { "/search", search_tests, NULL, 1, MUNIT_SUITE_OPTION_NONE },
     { NULL, NULL, NULL, 0, MUNIT_SUITE_OPTION_NONE } 
 };
-static const MunitSuite suite = { "/btree", NULL, sub_suites, 15, MUNIT_SUITE_OPTION_NONE };
+static const MunitSuite suite = { "/btree", NULL, sub_suites, 100, MUNIT_SUITE_OPTION_NONE };
 
 int main(int argc, char * const argv[]) {
     return munit_suite_main(&suite, NULL, argc, argv);
